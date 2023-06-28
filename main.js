9449214059e103911e6ae7e4d52148c0708f1463
ttiop2289 <127741157+ttiop2289@users.noreply.github.com>
2023-06-28 12:01:59 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var changeBackgroundBtn = document.getElementById("changeBackgroundBtn");
+
+changeBackgroundBtn.addEventListener("click", function() {
+  var body = document.querySelector("body");
+  body.style.backgroundColor = "brown"; 
+});
+
+var changeBackgroundBtn1 = document.getElementById("changeBackgroundBtn1");
+
+changeBackgroundBtn1.addEventListener("click", function() {
+  var body = document.querySelector("body");
+  body.style.backgroundColor = "white"; 
+});
+
+var changeBackgroundBtn2 = document.getElementById("changeBackgroundBtn2");
+
+changeBackgroundBtn2.addEventListener("click", function() {
+  var body = document.querySelector("body");
+  body.style.backgroundColor = "gray"; 
+});
